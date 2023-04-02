1b965c69e578177144151bcecb27e9d60a4ea005
Yassr HADJ ABDOU RAZACK <yassrhadj@gmail.com>
2023-04-02 14:23:46 +0100

    P2C2 (Gagnez en temps et en efficacité grâce aux listes et aux conditions) exercise 2 done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,27 +1,25 @@
 import { plantList } from "../datas/plantList";
+import "../styles/ShoppingList.css";
 
 function ShoppingList() {
-  const categories = plantList.reduce((acc, obj) => {
-    if (!acc.includes(obj.category)) {
-      acc.push(obj.category);
-    }
-    return acc;
-  }, []);
-  console.log(categories);
+  const categories = plantList.reduce(
+    (acc, plant) =>
+      acc.includes(plant.category) ? acc : acc.concat(plant.category),
+    []
+  );
 
   return (
     <div>
       <ul>
-        {categories.map((category) => (
-          <li key={`${category}`}>{category}</li>
+        {categories.map((cat) => (
+          <li key={cat}>{cat}</li>
         ))}
       </ul>
-
-      <ul>
+      <ul className="lmj-plant-list">
         {plantList.map((plant) => (
-          // <li key={`${plant.id}`}>{plant.name} {plant.isBestSale ? <span>🔥</span> : null}</li>
-          <li key={`${plant.id}`}>
-            {plant.name} {plant.isBestSale && <span>🔥</span>}
+          <li key={plant.id} className="lmj-plant-item">
+            {plant.name}{" "}
+            {plant?.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
           </li>
         ))}
       </ul>
